6605fea3d18e46adf2b01f18530d2c3c30664d99
Maaz-Bukhari <maazbukhari1144@gmail.com>
2025-10-05 14:09:25 +0500

    horizontal scroll cards added!

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { FeaturedEvents } from "./components/FeaturedEvents";
 import { Header } from "./components/Header";
+import StickyTextParallax from "./components/StickyTextParallax/StickyTextParallax";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <FeaturedEvents />
+      <StickyTextParallax />
     </>
   );
 }
