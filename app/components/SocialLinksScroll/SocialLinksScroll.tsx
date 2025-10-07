2f5d71112dc522088969788025a42012db9c3db2
Bilal Ahmad <bilal.codemaster@gmail.com>
2025-10-07 10:22:41 +0500

    social icon and text added

---
+++ app/components/SocialLinksScroll/SocialLinksScroll.tsx
@@ -0,0 +1,124 @@
+import "./SocialLink.css"
+
+function SocialLinksScroll() {
+    return (
+        <div className="style_social__platform__9gGBg ">
+            {/* Instagram */}
+            <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="style_social__icon__wrapper__QhDdv"
+                aria-label="instagram"
+            >
+                <svg
+                    width="65"
+                    height="65"
+                    viewBox="0 0 65 65"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    focusable="false"
+                >
+                    <g filter="url(#filter0_d_663_9293)">
+                        <rect
+                            x="2"
+                            y="2"
+                            width="58"
+                            height="58"
+                            rx="29"
+                            fill="black"
+                            fillOpacity="0.22"
+                            shapeRendering="crispEdges"
+                        />
+                        <rect
+                            x="3"
+                            y="3"
+                            width="56"
+                            height="56"
+                            rx="28"
+                            stroke="url(#paint0_linear_663_9293)"
+                            strokeWidth="2"
+                            shapeRendering="crispEdges"
+                        />
+                        <path
+                            d="M39.0873 21.1943C38.0963 21.1943 37.299 21.9917 37.299 22.9826C37.299 23.9736 38.0963 24.7709 39.0873 24.7709C40.0783 24.7709 40.8756 23.9736 40.8756 22.9826C40.8756 21.9917 40.0783 21.1943 39.0873 21.1943Z"
+                            fill="white"
+                        />
+                        <path
+                            d="M31.1246 23.4834C26.9785 23.4834 23.6069 26.855 23.6069 31.0011C23.6069 35.1472 26.9785 38.5187 31.1246 38.5187C35.2707 38.5187 38.6422 35.1472 38.6422 31.0011C38.6422 26.855 35.2707 23.4834 31.1246 23.4834ZM31.1246 35.8192C28.4706 35.8192 26.3064 33.655 26.3064 31.0011C26.3064 28.3471 28.4706 26.1829 31.1246 26.1829C33.7786 26.1829 35.9427 28.3471 35.9427 31.0011C35.9427 33.655 33.7786 35.8192 31.1246 35.8192Z"
+                            fill="white"
+                        />
+                        <path
+                            d="M37.0938 46.2651H24.9061C19.8488 46.2651 15.7368 42.1531 15.7368 37.0958V24.9081C15.7368 19.8507 19.8488 15.7388 24.9061 15.7388H37.0938C42.1512 15.7388 46.2631 19.8507 46.2631 24.9081V37.0958C46.2631 42.1531 42.1512 46.2651 37.0938 46.2651ZM24.9061 18.6092C21.432 18.6092 18.6072 21.434 18.6072 24.9081V37.0958C18.6072 40.5699 21.432 43.3947 24.9061 43.3947H37.0938C40.5679 43.3947 43.3927 40.5699 43.3927 37.0958V24.9081C43.3927 21.434 40.5679 18.6092 37.0938 18.6092H24.9061Z"
+                            fill="white"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_d_663_9293"
+                            x="-28.6349"
+                            y="-28.6349"
+                            width="119.27"
+                            height="119.27"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feColorMatrix
+                                in="SourceAlpha"
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                result="hardAlpha"
+                            />
+                            <feOffset dx="1.53175" dy="1.53175" />
+                            <feGaussianBlur stdDeviation="1.53175" />
+                            <feComposite in2="hardAlpha" operator="out" />
+                            <feColorMatrix
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"
+                            />
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_663_9293" />
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_663_9293" result="shape" />
+                        </filter>
+                        <linearGradient
+                            id="paint0_linear_663_9293"
+                            x1="31"
+                            y1="2"
+                            x2="31"
+                            y2="60"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stopColor="#2911FF" />
+                            <stop offset="1" stopColor="#A812FF" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+            </a>
+
+            {/* YouTube */}
+            <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="style_social__icon__wrapper__QhDdv"
+                aria-label="youtube"
+            >
+                <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><g filter="url(#filter0_d_663_9291)"><rect x="2" y="2" width="58" height="58" rx="29" fill="black" fill-opacity="0.22" shape-rendering="crispEdges"></rect><rect x="3" y="3" width="56" height="56" rx="28" stroke="url(#paint0_linear_663_9291)" stroke-width="2" shape-rendering="crispEdges"></rect><path d="M43.3575 27.6775C43.3575 24.4892 40.9543 21.9136 37.9795 21.9136H24.2275C21.2527 21.9136 18.8495 24.4892 18.8495 27.6775V34.5306C18.8495 37.7189 21.2527 40.2945 24.2275 40.2945H37.9795C40.9543 40.2945 43.3575 37.7189 43.3575 34.5306V27.6775ZM35.2693 31.626L29.1079 34.8937C28.8645 35.0299 28.0493 34.8483 28.0493 34.5533V27.8363C28.0493 27.5413 28.875 27.3484 29.1185 27.4959L35.0258 30.9339C35.2693 31.0814 35.534 31.4671 35.2799 31.6146L35.2693 31.626Z" fill="white"></path></g><defs><filter id="filter0_d_663_9291" x="-28.6349" y="-28.6349" width="119.27" height="119.27" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset dx="1.53175" dy="1.53175"></feOffset><feGaussianBlur stdDeviation="1.53175"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"></feColorMatrix><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_663_9291"></feBlend><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_663_9291" result="shape"></feBlend></filter><linearGradient id="paint0_linear_663_9291" x1="31" y1="2" x2="31" y2="60" gradientUnits="userSpaceOnUse"><stop stop-color="#2911FF"></stop><stop offset="1" stop-color="#A812FF"></stop></linearGradient></defs></svg>
+            </a>
+
+            {/* LinkedIn */}
+            <a
+                href="https://www.linkedin.com/company/"
+                target="_blank"
+                rel="noreferrer"
+                className="style_social__icon__wrapper__QhDdv"
+                aria-label="linkedin"
+            >
+                <svg width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><g filter="url(#filter0_d_860_12784)"><rect x="3" y="3" width="80" height="80" rx="40" fill="black" fill-opacity="0.22" shape-rendering="crispEdges"></rect><rect x="4.37931" y="4.37931" width="77.2414" height="77.2414" rx="38.6207" stroke="url(#paint0_linear_860_12784)" stroke-width="2.75862" shape-rendering="crispEdges"></rect><path d="M59.9419 47.1385V59.0573C59.9419 59.35 59.6902 59.6009 59.3967 59.6009H53.2322C52.9386 59.6009 52.687 59.35 52.687 59.0573V47.9749C52.687 45.0475 51.6386 43.0401 48.9967 43.0401C46.9838 43.0401 45.8096 44.3783 45.2644 45.6748C45.0548 46.1348 45.0128 46.8039 45.0128 47.4312V59.0573C45.0128 59.35 44.7612 59.6009 44.4677 59.6009H38.3451C38.0515 59.6009 37.7999 59.35 37.7999 59.0573C37.7999 56.088 37.8838 41.7437 37.7999 38.398C37.7999 38.1053 38.0515 37.8544 38.3451 37.8544H44.5096C44.8032 37.8544 45.0548 38.1053 45.0548 38.398V40.9491C45.0548 40.9909 45.0128 40.9909 45.0128 41.0327H45.0548V40.9491C46.0193 39.4854 47.7386 37.3525 51.5967 37.3525C56.3773 37.3525 59.9419 40.4891 59.9419 47.1385ZM27.0644 59.6427H33.229C33.5225 59.6427 33.7741 59.3918 33.7741 59.0991V38.398C33.7741 38.1053 33.5225 37.8544 33.229 37.8544H27.0644C26.7709 37.8544 26.5193 38.1053 26.5193 38.398V59.0991C26.5612 59.3918 26.7709 59.6427 27.0644 59.6427Z" fill="white"></path><path d="M29.9581 35.136C32.112 35.136 33.8581 33.3947 33.8581 31.2467C33.8581 29.0987 32.112 27.3574 29.9581 27.3574C27.8042 27.3574 26.0581 29.0987 26.0581 31.2467C26.0581 33.3947 27.8042 35.136 29.9581 35.136Z" fill="white"></path></g><defs><filter id="filter0_d_860_12784" x="-39.2551" y="-39.2551" width="164.51" height="164.51" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset dx="2.11275" dy="2.11275"></feOffset><feGaussianBlur stdDeviation="2.11275"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"></feColorMatrix><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_860_12784"></feBlend><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_860_12784" result="shape"></feBlend></filter><linearGradient id="paint0_linear_860_12784" x1="43" y1="3" x2="43" y2="83" gradientUnits="userSpaceOnUse"><stop stop-color="#2911FF"></stop><stop offset="1" stop-color="#A812FF"></stop></linearGradient></defs></svg>
+            </a>
+        </div>
+    );
+}
+
+export default SocialLinksScroll
