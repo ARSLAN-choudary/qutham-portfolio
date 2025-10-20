dcd09447023fe57eb9f9919c279f6ea33b99003d
Arslan-Choudary <arslanchoudary23@gmail.com>
2025-10-20 14:55:42 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config optio */
+  reactStrictMode: true,
+  output: "standalone",
 };
 
 export default nextConfig;
